8c7fdcae50a61d44d114e70eb3cc52ea366185d4
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-04-27 16:10:27 +0300

    Correct test cases for overloaded constructor

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Test test1 = new Test();
         test1.test2 = new Test2();
+	Test test3 = new Test(test1, new Test2(), new Test2());
         test1.speak(new Test2());
     }
     
