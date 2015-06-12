69aca7d2320c1caa2b5923ac76f43e9f5939b9d1
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-06-12 19:12:11 +0300

    Changes

--- Main.java
+++ Main.java
@@ -6,7 +6,13 @@ public class Main {
         Test test1 = new Test();
         test1.test2 = new Test2();
         Test test3 = new Test(test1, new Test2(), new Test2());
+        test1 = foo();
         test1.speak(new Test2());
+    }
+
+
+    static Test foo() {
+	return new Test();
     }
     
 }
