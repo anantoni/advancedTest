2c3fa91850dcd431b0e0d42a4a59335c9820364f
Anastasios Antoniadis <anastasios.antoniadis@gmail.com>
2015-06-17 23:10:26 +0300

    Add build.gradle

--- Main.java
+++ Main.java
@@ -8,9 +8,29 @@ public class Main {
         Test test3 = new Test(test1, new Test2(), new Test2());
         test1 = foo();
         test1.speak(new Test2());
+        test3.speak(test1.test2);
+
+        Test2 test2 = bar(test1.test2);
+        test2.speak();
+        int a = 1;
+        test1 = new Main().buz(a, test3, test2);
+        test1.speak();
     }
 
-    static Test foo() {
+    public static Test foo() {
+        return new Test();
+    }
+
+    public static Test2 bar(Test2 arg) {
+        if (arg == null)
+            return new Test2();
+        else
+            return arg;
+    }
+
+    public Test buz(int a, Test b, Test2 c) {
+        b.speak(c);
+        c.speak();
         return new Test();
     }
 }
